3927bafcbef31317e23763fb4b2ce49471eaac1a
Jirka Stybnar <Jirka.Stybnar@ImageAccess.com>
2022-02-11 18:38:37 -0500

    3.8.14.4

--- BIP/Properties/AssemblyInfo.cs
+++ BIP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-[assembly: AssemblyVersion("3.8.14.3")]
+[assembly: AssemblyVersion("3.8.14.4")]
 
 [assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("C:\\Documents and Settings\\Jirka\\My Documents\\prog\\ImageProcessing\\ImageProcessing\\ImageProcessing.BigImages.snk")]
